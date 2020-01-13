9ff6c7a9fc3feadf056c2dfe13f2e6b5dc72676e
Sean Prashad <S.Prashad@outlook.com>
2020-01-12 23:35:38 -0500

    Updates

---
+++ precache-manifest.14713cba968e2443544867d8464bb99f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8126e489f4895e9f2f8b711bbd577726",
+    "url": "/leetcode-patterns/index.html"
+  },
+  {
+    "revision": "731184af84b0513667f8",
+    "url": "/leetcode-patterns/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3d649a7bf1dead6ec99e",
+    "url": "/leetcode-patterns/static/css/main.74187275.chunk.css"
+  },
+  {
+    "revision": "731184af84b0513667f8",
+    "url": "/leetcode-patterns/static/js/2.66f880b3.chunk.js"
+  },
+  {
+    "revision": "c93c8ec49ddf5bc7eb83c9836aeb13e5",
+    "url": "/leetcode-patterns/static/js/2.66f880b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d649a7bf1dead6ec99e",
+    "url": "/leetcode-patterns/static/js/main.38c0b921.chunk.js"
+  },
+  {
+    "revision": "88d3b214b0e7593a2670",
+    "url": "/leetcode-patterns/static/js/runtime-main.6e55e825.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/leetcode-patterns/precache-manifest.e826b6ad7a30acbd9383f0ac4c2c85da.js"
+  "/leetcode-patterns/precache-manifest.14713cba968e2443544867d8464bb99f.js"
 );
 
 self.addEventListener('message', (event) => {
