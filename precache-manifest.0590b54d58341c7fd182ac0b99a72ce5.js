6abd7338df8910faae71fd6a5b5c1f53da917cee
Sean Prashad <S.Prashad@outlook.com>
2020-05-11 00:38:18 -0400

    Updates

--- precache-manifest.0590b54d58341c7fd182ac0b99a72ce5.js
+++ precache-manifest.0590b54d58341c7fd182ac0b99a72ce5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f5ae05ba9c31883263aa601c97914224",
+    "revision": "4938542abbd18ac4a080562286aa9272",
     "url": "/leetcode-patterns/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/leetcode-patterns/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "fe15d5d9a525c4d7e3b0",
-    "url": "/leetcode-patterns/static/css/main.4ee630f2.chunk.css"
+    "revision": "c9a31e741eea43f95676",
+    "url": "/leetcode-patterns/static/css/main.3339937d.chunk.css"
   },
   {
     "revision": "aec59aa91782f3057e4f",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/leetcode-patterns/static/js/2.c8bd0a72.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fe15d5d9a525c4d7e3b0",
-    "url": "/leetcode-patterns/static/js/main.6b3f0b1b.chunk.js"
+    "revision": "c9a31e741eea43f95676",
+    "url": "/leetcode-patterns/static/js/main.2fe7d00c.chunk.js"
   },
   {
     "revision": "01945b03266eb5b191fd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/leetcode-patterns/precache-manifest.1615b15b331612c5b0cc6b4866645ea1.js"
+  "/leetcode-patterns/precache-manifest.0590b54d58341c7fd182ac0b99a72ce5.js"
 );
 
 self.addEventListener('message', (event) => {
